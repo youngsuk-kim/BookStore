eb93e6fb34dbf634bd6dd5c7be35fff27676097f
youngsuk <ys@gcnco.com>
2025-01-16 13:07:40 +0900

    feat: rental 외래키 추가

--- app/book/domain/entity/rental.py
+++ app/book/domain/entity/rental.py
@@ -1,6 +1,8 @@
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from app.book.domain.entity.book import Book
+from app.user.domain.entity.user import User
 from core.db import Base
 from core.db.mixins import TimestampMixin
 
@@ -13,8 +15,14 @@ class Rental(Base, TimestampMixin):
     __tablename__ = "rental"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     description: str = Column(String(255), nullable=False)
-    user_id: Mapped[int] = mapped_column()
-    book_id: Mapped[int] = mapped_column()
+    user_id: Mapped[int] = Column(
+		Integer,
+        ForeignKey(User.id, ondelete="SET NULL", onupdate="CASCADE")
+    )
+    book_id: Mapped[int] = Column(
+		Integer,
+        ForeignKey(Book.id, ondelete="SET NULL", onupdate="CASCADE")
+    )
     user = relationship("User", back_populates="rentals")
     book = relationship("Book", back_populates="rentals")
 
